b20690d76d67fdc498bd394ff9b50af91a88e587
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2023-11-09 23:14:24 -0600

    add pseudocode

---
+++ main.cpp
@@ -0,0 +1,5 @@
+// display current balance
+// two input fields
+// two buttons, one Deposit and one Withdraw
+// below each is the date and amount of the most recent entry of that type
+// maybe figure out how to add a sound to a button click
