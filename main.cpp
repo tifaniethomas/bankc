5a5f1d61afb2caf7bd3135bb973436070208f39d
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2023-11-29 22:59:50 -0600

    install react and begin setup

--- main.cpp
+++
@@ -1,5 +0,0 @@
-// display current balance
-// two input fields
-// two buttons, one Deposit and one Withdraw
-// below each is the date and amount of the most recent entry of that type
-// maybe figure out how to add a sound to a button click
